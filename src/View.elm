f079299ba53685c3a90f9d641c81230296415bac
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2018-07-02 23:43:26 +0900

    Add styles

--- src/View.elm
+++ src/View.elm
@@ -3,18 +3,16 @@ module View exposing (view)
 import Json.Decode exposing (Decoder, at, string, map)
 import List exposing (range, length)
 import List.Extra exposing (zip)
-import Html exposing (div, text, Html, Attribute)
+import Html exposing (div, text, i, Html, Attribute)
 import Html.Events exposing (onClick, onFocus, onBlur, onInput, on)
-import Html.Attributes exposing (contenteditable)
+import Html.Attributes exposing (id, class, contenteditable)
 import Model exposing (Model, Msg(..), Item(..), ItemPath, defaultPath)
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [] [ text <| "focusing on: " ++ toString model.focus ]
-        , div [] <| List.map (viewItem []) (numerate model.items)
-        ]
+    div [ id "page" ]
+        [ div [] <| List.map (viewItem []) (numerate model.items) ]
 
 
 viewItem : ItemPath -> ( Int, Item ) -> Html Msg
@@ -24,29 +22,38 @@ viewItem parent ( n, Item item ) =
             n :: parent
     in
         div
-            []
+            [ class "item-container" ]
             [ div
-                [ onClick <| Toggle path ]
-                [ if item.expanded then
-                    text "-"
-                  else
-                    text "+"
+                [ class "item-toggle"
+                , onClick <| Toggle path
                 ]
-            , div [ onClick <| Indent path ] [ text ">>" ]
-            , div [ onClick <| Unindent path ] [ text "<<" ]
+                [ if item.expanded then
+                    i [ class "far fa-minus-square" ] []
+                  else
+                    i [ class "far fa-plus-square" ] []
+                ]
             , div
-                [ onFocus <| Focus path
+                [ class "item-bullet" ]
+                [ if item.expanded then
+                    i [ class "fas fa-angle-right" ] []
+                  else
+                    i [ class "fas fa-angle-double-right" ] []
+                ]
+            , div
+                [ class "item-content"
+                , onFocus <| Focus path
                 , onBlur <| Focus defaultPath
                 , onContentInput <| SetContents path
                 , contenteditable True
                 ]
-                [ text <| toString path ++ item.contents ]
-            , div []
-                (if item.expanded then
-                    List.map (viewItem path) (numerate item.children)
-                 else
-                    []
-                )
+                [ text item.contents
+                , div []
+                    (if item.expanded then
+                        List.map (viewItem path) (numerate item.children)
+                     else
+                        []
+                    )
+                ]
             ]
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,21 @@ module.exports = {
                 loader: 'file-loader?name=[name].[ext]'
             },
             {
-                test: /\.scss$/,
+                test: /\.(css|scss)$/,
                 exclude: /node_modules/,
                 use: [
                     'style-loader',
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
             },
             {
                 test: /\.elm$/,
